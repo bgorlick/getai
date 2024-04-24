76e65f6e40ca2452861638b1cc74b82507b54d5f
Benjamin Gorlick <ben@unifiedlearning.ai>
2024-04-24 05:49:05 -0700

    Split dataset search into two files to support making it a more usable lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='getai',
-    version='0.0.9',
+    version='0.0.91',
     author='Ben Gorlick',
     author_email='ben@unifiedlearning.ai',
     description='GetAI - Asynchronous AI Downloader for models, datasets and tools',
